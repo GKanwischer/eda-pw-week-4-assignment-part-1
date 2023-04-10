4bc7ba73e5427f29036dbca917bbd04985eee2b0
Garret Kanwischer <kanwischer.garret@gmail.com>
2023-04-10 17:55:01 -0500

    Completed stretch goals

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -89,24 +89,78 @@ console.log( 'The value exsists inside the array:', find( 9, [2, 3, 5, 8] ));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
+  return string.charAt(0) === letter;
 
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll( array ) {
   let sum = 0
+  for( let i = 0; i < array.length; i++ ){
+    sum += array[i];
+  }
   // TODO: loop to add items
   return sum;
 }
 
+console.log( 'The sum of the array is:', sumAll( [2, 5, 67] ));
+
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+function positiveArray( array ){
+  let newArray = [];
+  for( let i = 0; i < array.length; i++ ){
+    if (array[i] > 0){
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
 
-
+console.log( 'The positive numbers in the array are:', positiveArray( [1, -2, 3, -4, 5, -6, 7] ));
+console.log( 'The positive numbers in the array are:', positiveArray( [-1, -2, -3, -4, -5, -6, -7] ));
+console.log( 'The positive numbers in the array are:', positiveArray( [-1, -2, 3, 4, -5, -6, 7, 0] ));
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+// 11a. Create a function that takes a number as an argument. 
+// Add up all the numbers from 1 to the number you passed to the function.
+
+function numSum (number){
+  let sum = 0;
+  for( let i = number; i >= 1 ; i-- ){
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(numSum( 3 ));
+console.log(numSum( 8 ));
+console.log(numSum( 10 ));
+console.log(numSum( 23 ));
+
+// 11b. Create a function that takes an array of numbers and return both 
+// the minimum and maximum numbers, in that order.
+
+function minMaxArray ( array ){
+  let min = array[0];
+  let max = array[0];
+
+  for ( let i = 0; i < array.length; i++ ){
+    if (array[i] < min){
+      min = array[i];
+    }
+    if (array[i] > max){
+      max = array[i];
+    }
+  }
+  return [min, max];
+}
+
+console.log( 'The smallest and largest numbers in the array are:', minMaxArray( [10, 23, 4, 891, 6, -3] ));
+console.log( 'The smallest and largest numbers in the array are:', minMaxArray( [1, 2, 3, 4, 5, 6, 7] ));
